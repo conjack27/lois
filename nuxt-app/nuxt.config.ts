4733db7e48d8b571403924e6d49bbc45712dd0f5
conjack27 <38353145+conjack27@users.noreply.github.com>
2023-12-01 14:19:37 +0100

    Update nuxt.config.ts

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    target: "static",
     devtools: { enabled: true },
     modules: ["@nuxtjs/google-fonts"],
     css: ["~/assets/css/main.css"],
