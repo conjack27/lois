5c24be4a0d43e3720132e94ac043fb8821936197
conjack27 <38353145+conjack27@users.noreply.github.com>
2023-12-01 14:05:18 +0100

    updated bg color

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,20 +1,16 @@
 <template>
-    <div class="in-progress">
-        <h1>Hello!</h1>
-        <h2>
-            My website is still in progress, but I hope to have something done
-            soon.
-        </h2>
-        <h2>
-            If you would like to contact me in the meantime, please find me
-            here:
-        </h2>
-        <a
-            class="my-email"
-            href="mailto:loisnotebaart@gmail.com"
-            target="_blank"
-            >loisnotebaart@gmail.com</a
-        >
-        <!-- <video class="main-video" autoplay loop muted></video> -->
+    <div class="main">
+        <div class="in-progress">
+            <h1>Hello, my website is being changed.</h1>
+            <h2>Will be online again shortly!</h2>
+            <h2>If you need to contact me here:</h2>
+            <a
+                class="my-email"
+                href="mailto:loisnotebaart@gmail.com"
+                target="_blank"
+                >loisnotebaart@gmail.com</a
+            >
+            <!-- <video class="main-video" autoplay loop muted></video> -->
+        </div>
     </div>
 </template>
